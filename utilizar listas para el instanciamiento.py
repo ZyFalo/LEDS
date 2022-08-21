634bf7210ccb0a7c9924b64eb0b4c1cd857a5e9e
ZyFalo <williamandres1603@gmail.com>
2022-08-21 12:04:15 -0500

    Se agrega el Proyecto

---
+++ utilizar listas para el instanciamiento.py
@@ -0,0 +1,31 @@
+#*************************Forma 5: utilizar listas para el instanciamiento.**********************************
+
+from machine import Pin as pin
+from utime import sleep as pausa, sleep_ms as pausam, sleep_us as pausau
+
+puerto = [15, 2, 4, 16, 5, 26, 19, 21, 22, 23]
+instancias = []
+
+for i in puerto:
+  instancias.append (pin(i, pin.OUT))
+
+print (instancias)
+
+inicio = 0
+fin = 11
+def derecha_a_izquierda(inicio, fin):
+  for i in instancias[inicio:fin]:
+    for j in range (2):
+      i.value(not i.value())
+    pausam(350)
+
+def izquierda_a_derecha(inicio, fin):
+  for i in reversed (instancias[inicio:fin]):
+    for j in range (2):
+      i.value(not i.value())
+    pausam(350)
+
+
+while True:
+  derecha_a_izquierda(inicio, fin)
+  # izquierda_a_derecha(inicio, fin)
